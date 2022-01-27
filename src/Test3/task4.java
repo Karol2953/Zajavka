e43f0ef24ddcd14a52deaad3de2983768e693c12
Karol <you@example.com>
2022-01-27 15:26:38 +0100

    work12

--- src/Test3/task4.java
+++ src/Test3/task4.java
@@ -1,11 +1,28 @@
 package Test3;
 
-//Rafał ma 6 kolorowych mazaków (czerwony, zielony, niebieski, czarny, żółty i brązowy) i rysuje nimi
-//w podanej kolejności kolorów 100 kresek. Jaki kolor będzie miała ostatnia kreska? Napisz program,
-//który to policzy.
-
+/*5. Napisz metodę, która jako argument przyjmuje liczbę i wydrukuje informację czy metoda jest
+        podzielna przed 3, przez 7 oraz przez 3 i 7 jednocześnie.*/
 public class task4 {
     public static void main(String[] args) {
 
+        someMethod(35);
+        someMethod(24);
+        someMethod(32);
+        someMethod(12);
+        someMethod(21);
+        someMethod(177);
+
+    }
+    public static void someMethod(int a) {
+        if (a%7 == 0 && a%3==0) {
+            System.out.println("liczba jest podzielna przez 3 i 7 jednoczesnie");
+        }
+        else if (a%3==0) {
+            System.out.println(" liczba podzielna jest tylko przez 3");
+        }
+
+        else if (a%7==0) {
+            System.out.println(" liczba podzielna jest tylko przez 7");
+        }
     }
 }
