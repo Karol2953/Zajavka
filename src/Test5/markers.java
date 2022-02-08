6b9e4a0a4b56cd3d11e2b12fbaf3a1b576a6a065
Karol <you@example.com>
2022-02-08 15:04:02 +0100

    work8222

---
+++ src/Test5/markers.java
@@ -0,0 +1,39 @@
+package Test5;
+
+//. Rafał ma 6 kolorowych mazaków (czerwony, zielony, niebieski, czarny, żółty i brązowy) i rysuje nimi
+//w podanej kolejności kolorów 100 kresek. Wypisz wszystkie kolory w kolejności odpowiadające
+//rysowanym kreskom i zobacz jaki kolor będzie ostatni.
+
+public class markers {
+    public static void main(String[] args) {
+
+        String [] markers = {"red", "green", "blue", "black", "yellow", "brown"};
+
+
+        int numberOfLine = 6;
+
+        double x = numberOfLine % markers.length;
+        System.out.println(x);
+
+        int x1 = numberOfLine / markers.length;
+        System.out.println(x1);
+
+
+
+        for (int i = 0; i < x1; i++) {
+            System.out.println("--------");
+            for (int j = 0; j < markers.length; j++) {
+                System.out.println(markers[j]);
+
+            }
+            System.out.println("------------------end----------------");
+        }
+        if (x == 0) System.out.println("red");
+        if (x == 1) System.out.println("green");
+        if (x == 2) System.out.println("blue");
+        if (x == 3) System.out.println("black");
+        if (x == 4) System.out.println("yellow");
+        if (x == 5) System.out.println("brown");
+
+    }
+}
--- src/Test5/tree.java
+++ src/Test5/tree.java
@@ -7,28 +7,19 @@ public class tree {
         String space = " ";
         String star2;
         String space2;
+        String space3;
 
-        int x1 = 8;   //Ilość znaków w podstawie bedzie (2*x1)
+        int x1 = 20;   //Ilość znaków w podstawie bedzie (2*x1)
 
         for (int i = 0; i <= x1; i++) {
             star2 = star.repeat(i);
             space2 = space.repeat(x1 - i);
-            System.out.println(space2 + star2 + star2);
+            space3 = space.repeat(x1 + i);
+            System.out.println(space2 + star2 + space3 );
         }
 
     }
-        /*for (int i = 10; i > 0 ; i--) {
-            space2 = space.repeat(i);
-            System.out.println(space2);
-        }*/
 
-
-
-
-      /*  int x = 3;
-
-        for (int i = x; i > 0 ; i--) {
-            System.out.print("x");*/
 
 
 }
