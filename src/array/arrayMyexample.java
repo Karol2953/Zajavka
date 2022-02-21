66b49939e3d62b40ff1688347bd9b4b2381c9251
Karol2953 <grudzio2953@gmail.com>
2022-02-21 21:46:01 +0100

    210222

--- src/array/arrayMyexample.java
+++ src/array/arrayMyexample.java
@@ -1,2 +1,21 @@
-package array;public class arrayMyexample {
+package array;
+
+import java.util.Arrays;
+
+public class arrayMyexample {
+    public static void main(String[] args) {
+        int [][] exampleArray = {
+                {11,  12,  13, 14},
+                {21, 22, 23, 24},
+                {31, 32, 33, 34},
+                {41, 42, 44, 44}
+        };
+        for (int i = 0; i < exampleArray.length; i++) {
+            for (int j = 0; j < exampleArray[i].length; j++) {
+                System.out.println("accesing" + "[" + i+ "]"+ "[" + j+ "]" + " " + exampleArray[i][j]);
+            }
+
+        }
+    }
 }
+ 
