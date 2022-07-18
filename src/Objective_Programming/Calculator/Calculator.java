4ca83530e822e782c467b8a0548f9c59e4bb800c
Karol2953 <grudzio2953@gmail.com>
2022-07-18 07:03:19 +0200

    18.07.2022

--- src/Objective_Programming/Calculator/Calculator.java
+++ src/Objective_Programming/Calculator/Calculator.java
@@ -1,2 +1,20 @@
-package Objective_Programming.Calculator;public class Calculator {
+package Objective_Programming.Calculator;
+
+public class Calculator {
+    public String name;
+
+    public Calculator(String name){
+        this.name = name;
+    }
+
+        public boolean equals(Object obj){
+        if (obj == null || !(obj instanceof Calculator)) {        // !different object than Calculator
+            return false;
+        }
+        if (!(obj instanceof Calculator)){
+           return false;
+        }
+
+        return false;
+    }
 }
--- src/Objective_Programming/Calculator/Main.java
+++ src/Objective_Programming/Calculator/Main.java
@@ -1,2 +1,11 @@
-package Objective_Programming.Calculator;public class Main {
+package Objective_Programming.Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc1 = new Calculator("Sony");
+        Calculator calc2 = new Calculator("Sony");
+
+        System.out.println(calc1.equals(calc2));
+
+    }
 }
