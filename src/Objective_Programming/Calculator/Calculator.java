49eca51b77e15d41620dd4e11dbe4029cf7b2afb
Karol2953 <grudzio2953@gmail.com>
2022-07-19 06:55:58 +0200

    a

--- src/Objective_Programming/Calculator/Calculator.java
+++ src/Objective_Programming/Calculator/Calculator.java
@@ -1,20 +1,35 @@
 package Objective_Programming.Calculator;
 
+import java.util.Objects;
+
 public class Calculator {
     public String name;
 
-    public Calculator(String name){
-        this.name = name;
+//    public Calculator(String name){
+//        this.name = name;
+//    }
+//        public boolean equals(Object obj){
+//        if (this ==obj) {
+//            return true;
+//        }
+//        if (obj == null || !(obj instanceof Calculator)) {        // !different object than Calculator
+//            return false;
+//        }
+//        Calculator calculator = (Calculator) obj;
+//        return calculator.name.equals(this.name);
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Calculator that = (Calculator) o;
+        return Objects.equals(name, that.name);
     }
 
-        public boolean equals(Object obj){
-        if (obj == null || !(obj instanceof Calculator)) {        // !different object than Calculator
-            return false;
-        }
-        if (!(obj instanceof Calculator)){
-           return false;
-        }
-
-        return false;
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }
