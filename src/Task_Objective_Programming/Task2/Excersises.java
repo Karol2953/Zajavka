8e36008073ee31c646aae4a1e43a265f44d5bd20
Karol2953 <grudzio2953@gmail.com>
2022-10-19 06:28:16 +0200

    19.10.22

--- src/Task_Objective_Programming/Task2/Excersises.java
+++ src/Task_Objective_Programming/Task2/Excersises.java
@@ -1,6 +1,6 @@
 package Task_Objective_Programming.Task2;
 
-import java.util.function.Function;
+import java.util.Arrays;
 
 public class Excersises  {
     public static void main(String[] args) {
@@ -10,23 +10,34 @@ public class Excersises  {
 
     private static void exercise2() {
 
-      FunctionClass half = new HalfCalculator();
-      FunctionClass quarter = new QuarterCalculator();
+     /* FunctionClass half = new HalfCalculator();
+      FunctionClass quarter = new QuarterCalculator();*/
 
        /* System.out.println(half.calculate(50));
         System.out.println(quarter.calculate(80));
         System.out.println(half.calculate(100));
         System.out.println(quarter.calculate(120));*/
 
-        int start = 125467;
+        int start = 1200;
 
-        Function[] functions = new Function[10];
+        Function [] functions = new Function[10];
 
         for (int i = 0; i < functions.length; i++) {
-            if (i % 2 == 0) functions[i] = (Function) new HalfCalculator();
-            else functions[i] = (Function) new QuarterCalculator();
-            System.out.println(functions.toString());
+            if (i % 2 == 0) {
+                functions[i] =  new HalfCalculator();
+            }
+            else {
+                functions[i] =  new QuarterCalculator();
+            }
         }
+
+        System.out.println();
+
+        for (Function function : functions){
+            start += function.calculate(start);
+            System.out.println(start);
+        }
+    }
 
     }
 
@@ -36,4 +47,4 @@ public class Excersises  {
 
 
 
-}
+
--- src/Task_Objective_Programming/Task2/HalfCalculator.java
+++ src/Task_Objective_Programming/Task2/HalfCalculator.java
@@ -3,7 +3,7 @@ package Task_Objective_Programming.Task2;
 public class HalfCalculator extends FunctionClass implements Function {
     @Override
     public int calculate(int arg) {
-        return arg/2;
+        return arg / 2;
     }
     public HalfCalculator() {
         System.out.println("half");
